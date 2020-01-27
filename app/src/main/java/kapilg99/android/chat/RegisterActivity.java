6b8d2f342a6f2d5d855d76fbb82097fb143771d0
Kapil <kmgoff18@gmail.com>
2020-01-27 21:03:43 +0530

    Register user success

--- app/src/main/java/kapilg99/android/chat/RegisterActivity.java
+++ app/src/main/java/kapilg99/android/chat/RegisterActivity.java
@@ -1,22 +1,72 @@
 package kapilg99.android.chat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private TextInputLayout mDisplayName, mEmail, mPassword;
     private Button mCreateButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mAuth = FirebaseAuth.getInstance();
 
+        mDisplayName = findViewById(R.id.reg_display_name);
+        mEmail = findViewById(R.id.reg_email);
+        mPassword = findViewById(R.id.reg_password);
+        mCreateButton = findViewById(R.id.reg_create_account);
+
+        mCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String displayName = mDisplayName.getEditText().getText().toString();
+                String email = mEmail.getEditText().getText().toString();
+                String password = mPassword.getEditText().getText().toString();
+
+                registerUser(displayName, email, password);
+            }
+        });
+    }
+
+    private void registerUser(String displayName, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.e("RegisterActivity", "createUserWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_LONG).show();
+                    Intent sendToMain = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(sendToMain);
+                    finish();
+//                            updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.e("RegisterActivty", "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_LONG).show();
+//                            updateUI(null);
+                }
+
+            }
+        });
     }
 }
